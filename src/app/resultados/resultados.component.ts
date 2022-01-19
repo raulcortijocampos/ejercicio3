77a83ad0cf84c170564d622bbe1277831a40a9be
Raul <raulcortijocampos@gmail.com>
2022-01-19 16:41:27 +0100

    con servicio y subscripcion

--- src/app/resultados/resultados.component.ts
+++ src/app/resultados/resultados.component.ts
@@ -12,15 +12,28 @@ import { ResultadosServiceService } from './resultados-service.service';
 
 export class ResultadosComponent implements OnInit {
 
-  resultados: Promise<Resultados[]>;
-  
-  constructor(serviceInst: ResultadosServiceService) { 
-      this.resultados = serviceInst.getObservablePartidos().toPromise();
+  resultadosPromise: Promise<Resultados[]>;
+  resultadosObservable: Observable<Resultados[]>
+  resultados: Resultados[];
+  constructor(serviceInst: ResultadosServiceService) {
+    //una promesa la cual podemos pintar sin mas 
+    this.resultadosPromise = serviceInst.getObservablePartidos().toPromise();
+    //el observable para usarlo despues
+    this.resultadosObservable = serviceInst.getObservablePartidos();
+    // variable de vista "vacía"
+    this.resultados = [];
     }
 
   
 
   ngOnInit(): void {
+    this.resultadosObservable.subscribe((data) => {
+      data.forEach( (value : Resultados) => {
+        //let partido: Partido = new Partido(value.nombre, value.dipu, value.imagen);
+        this.resultados.push(value);
+      });
+
+    });
   }
 
 }
